04ac9b991a3efa14be93d94199b36a2afd52c087
james2002chen <46477673+james2002chen@users.noreply.github.com>
2021-09-30 16:10:45 +0800

    20210930

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,29 @@
 #include "vex.h"
 
 using namespace vex;
-const double radius=1.0;
+const double radius=0.4;
 const double theta1=M_PI/3.0,theta2=M_PI/6.0;
+double gear=100;
+const double nonlinerlizeParm = 0.5;//almost liner
+const double deadband=0.05;
+double normalize(double Value,double range){
+  return Value/range;
+}
+//Value normalized
+double dedeadband(double Value){
+  return fmax(Value-deadband,0)/(1-deadband);
+}
+double nonlinerlize(double Value){
+  return sin(M_PI/2*nonlinerlizeParm*Value)/sin(M_PI/2*nonlinerlizeParm);
+}
 void DriveChasis(double Vx, double Vy, double rotate){
   double speed1,speed2,speed3;
   speed1=Vx+rotate*radius;
   speed2=-Vx*cos(theta1)-Vy*sin(theta1)+rotate*radius;
   speed3=-Vx*sin(theta2)+Vy*cos(theta2)+rotate*radius;
-  Motor1.spin(directionType::fwd, speed3, velocityUnits::rpm);
-  Motor2.spin(directionType::fwd, speed1, velocityUnits::rpm);
-  Motor3.spin(directionType::fwd, speed2, velocityUnits::rpm);
-  return;
+  Motor1.spin(directionType::fwd, speed3*gear, velocityUnits::pct);
+  Motor2.spin(directionType::fwd, speed1*gear, velocityUnits::pct);
+  Motor3.spin(directionType::fwd, speed2*gear, velocityUnits::pct);
 }
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
@@ -39,6 +51,10 @@ int main() {
     Vx=Controller1.Axis4.value();
     Vy=Controller1.Axis3.value();
     rotate=Controller1.Axis1.value();
+    Vx=nonlinerlize(normalize(Controller1.Axis4.value(),128.0));
+    Vy=nonlinerlize(normalize(Controller1.Axis3.value(),128.0));
+    rotate=nonlinerlize(normalize(Controller1.Axis1.value(),128.0));
+    if(Controller1.ButtonL1.pressing()) gear=50.0; else gear=100.0; 
     DriveChasis(Vx,Vy,rotate);
   }
 }
